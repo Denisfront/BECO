5794bf43c7879560504bffe4197af719ee1ef136
Sannikov Denis <kaktus.Zzzz@mail.ru>
2020-03-20 05:53:36 +0700

    адаптив phones

--- src/scripts/menu.js
+++ src/scripts/menu.js
@@ -1,12 +1,12 @@
 const sidebarBurger = document.querySelector('.menu-burger');
 const sidebar = document.querySelector('.sidebar');
-const menuTitle = document.querySelector('.menu__title');
+const body = document.querySelector('body');
 
 
 sidebarBurger.addEventListener('click', () => {
     sidebarBurger.classList.toggle('active');
     sidebar.classList.toggle('active');
-
+    body.classList.toggle('body__hidden');
     if(sidebar.classList.contains('hidden')) {
         sidebar.classList.remove('hidden');
     } else {
